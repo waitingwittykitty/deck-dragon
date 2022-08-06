912d5ae552abaab879606b8d14d2623cf15e0072
waitingwittykitty <hryschiller@gmail.com>
2022-08-06 05:24:46 -0700

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,25 @@
+import logo from './assets/react-logo.svg';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.body}>
+      <header className={styles.header}>
+        <img src={logo} className={styles.logo} alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="text-blue-300"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
