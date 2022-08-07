bb8ea0856574fe5c5a25c2e8bd1101451c3e022c
waitingwittykitty <hryschiller@gmail.com>
2022-08-07 04:48:04 -0700

    chore<frontend>: add fonts and extend tailwindcss for specific colors and font-families

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,19 @@
+// eslint-disable-next-line
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yellow: '#FFF48C',
+        red: '#F64242',
+      },
+      fontFamily: {
+        alfa: ['Alfa Slab One', ...defaultTheme.fontFamily.sans],
+        courier: ['Courier Prime', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
