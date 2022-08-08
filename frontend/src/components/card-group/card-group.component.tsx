d0750b5d99b0784e63302d07fc03727b2f091897
waitingwittykitty <hryschiller@gmail.com>
2022-08-07 17:40:07 -0700

    refactor<frontend>: change animation in more readable way

--- frontend/src/components/card-group/card-group.component.tsx
+++ frontend/src/components/card-group/card-group.component.tsx
@@ -1,11 +1,31 @@
+import { useMemo } from 'react';
 import Card from '../card/card.component';
 import styles from './card-group.module.css';
 
 function CardGroup({ cards }: CardGroupProps) {
+  const { length } = cards;
+
+  const classNames = useMemo(() => {
+    if (length === 5) {
+      return ['-curved-2', '-curved-1', 'curved', 'curved-1', 'curved-2'];
+    }
+    if (length === 4) {
+      return ['-curved-1', 'curved', 'curved', 'curved-1'];
+    }
+    if (length === 3) {
+      return ['-curved-1', 'curved', 'curved-1'];
+    }
+    if (length === 2) {
+      return ['curved', 'curved'];
+    }
+
+    return ['curved'];
+  }, [length]);
+
   return (
     <ul className={styles.group}>
       {cards.map((card, index) => (
-        <Card key={index} className={styles[`curved-${cards.length}`]} card={card} />
+        <Card key={index} className={styles[classNames[index]]} card={card} />
       ))}
     </ul>
   );
